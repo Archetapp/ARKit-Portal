3fec3aa85e18d46b0dbbe4585d792a03610a4b1b
Jared Davidson (Archetapp) <archetapp@gmail.com>
2018-02-14 13:41:39 -0700

    Initial Commit

---
+++ Portal3Tests/Portal3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Portal3Tests.swift
+//  Portal3Tests
+//
+//  Created by Davidson Family on 2/14/18.
+//  Copyright © 2018 Davidson Family. All rights reserved.
+//
+
+import XCTest
+@testable import Portal3
+
+class Portal3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
